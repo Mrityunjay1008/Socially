b69d5192e2f755300b6659ad4988fe4f284bf4c6
Mrityunjaya <sharmagopal1306@gmail.com>
2026-05-10 13:52:51 +0530

    Resolved production error

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -3,7 +3,4 @@ import { ourFileRouter } from "./core";
 
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
-  config: {
-    callbackUrl: `${process.env.UPLOADTHING_URL}/api/uploadthing`,
-  },
 });
